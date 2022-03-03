236c0d4d720fe711e8b55142d6fa064376e4c652
mecuong <68834774+mecuong@users.noreply.github.com>
2022-03-03 11:33:18 +0700

    Update gitform.php

--- gitform.php
+++ gitform.php
@@ -152,8 +152,8 @@
              * Comment some line below because not need use merge conflict online by tool
              */
             // $_SESSION['need_resolve'] = $branch;
-            // sendMsg('END');
-            // exit;
+            sendMsg('END');
+            exit;
         }
     }
 
@@ -403,7 +403,7 @@
                     icon.removeClass('fa-spin');
                     eventSource.close();
                 } else if (e.data.includes('CONFLICT:')) {
-                    // haveConflict(e.data.split('CONFLICT:')[1].split(','));
+                    haveConflict(e.data.split('CONFLICT:')[1].split(','));
                 } else if (e.data.includes('NEED RESOLVE BRANCH')) {
                     needResolveBranch(e.data.split('NEED RESOLVE BRANCH:')[1]);
                 } else {
@@ -424,7 +424,7 @@
         function needResolveBranch(branch = '') {
             $('#conflict-block').removeClass('invisible');
             var conflictFileListDom = $('#conflict-files');
-            conflictFileListDom.html( `<div class="alert alert-primary d-flex align-items-center" role="alert">
+            conflictFileListDom.append(`<div class="alert alert-primary d-flex align-items-center" role="alert">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                 </svg>
